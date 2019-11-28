693f5458aa5ed8c0823df87b24cb3a9909a2a2b7
2class-012 <2class-012@2class-012-PC>
2019-11-28 16:36:34 +0900

    123i

---
+++ Pro/src/BoardUi/BoardDAO.java
@@ -0,0 +1,11 @@
+package BoardUi;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class BoardDAO {
+	Connection con;
+	PreparedStatement ps;
+	ResultSet rs;
+}
