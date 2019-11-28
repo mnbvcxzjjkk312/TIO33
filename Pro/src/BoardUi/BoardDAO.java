6f1189981b0dbb2a7d488efef65c6600f38ef542
2class-012 <2class-012@2class-012-PC>
2019-11-28 17:02:48 +0900

    오류 뭐임

--- Pro/src/BoardUi/BoardDAO.java
+++ Pro/src/BoardUi/BoardDAO.java
@@ -3,9 +3,30 @@ package BoardUi;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class BoardDAO {
 	Connection con;
 	PreparedStatement ps;
 	ResultSet rs;
+	
+	public int insertUser(BoardVO user) {
+		try {
+			con=DBUtil.getCon();
+			String sql= "insert into memo values(user_seq.nextval,?,?,?)";
+			ps=con.prepareStatement(sql);
+			ps.setString(1, user.getId());
+			ps.setString(2, user.getPw());
+			ps.setInt(3, user.getGr());
+			int n = ps.executeUpdate();
+			return n;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return -1;//오류 발생시는 음수를 반환
+		}finally {
+			close();
+		}
+	}//--------------------------------------
+	
+	
 }
