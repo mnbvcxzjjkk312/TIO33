578f8174e754220048e9a6ba00333af5386d1ac9
SK O <mnbvcxzjjkk312@hotmail.com>
2019-11-29 09:42:52 +0900

    작성자 : 오성규 delete 메서드 구현.

---
+++ Pro/src/BoardUi/BoardDAO.java
@@ -0,0 +1,54 @@
+package BoardUi;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class BoardDAO
+{
+	Connection con;
+	PreparedStatement ps;
+	ResultSet rs;
+	
+	
+	
+	
+	
+	public int delete(Integer boardnum)
+	{
+		try
+		{
+			con = DBConnection.getCon();
+			String sql = "delete from board where boardnum = ?";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, boardnum);
+			ps.execute();
+			return boardnum;
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return -1;
+		}
+		finally
+		{
+			close();
+		}
+	}
+	
+	
+	
+	private void close()
+	{
+		try
+		{
+			if(rs != null) rs.close();
+			if(ps != null) ps.close();
+			if(con != null) con.close();
+		} 
+		catch (Exception e)
+		{
+
+		}
+	}
+}
