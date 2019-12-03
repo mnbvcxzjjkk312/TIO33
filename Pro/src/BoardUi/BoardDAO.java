0487f6775f5ff0eab6dac8c6521012763c2058a3
2class-012 <2class-012@2class-012-PC>
2019-12-03 13:57:18 +0900

    코딩중

--- Pro/src/BoardUi/BoardDAO.java
+++ Pro/src/BoardUi/BoardDAO.java
@@ -4,16 +4,17 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class BoardDAO {
 	Connection con;
 	PreparedStatement ps;
 	ResultSet rs;
 	
-	public int insertUser(BoardVO user) {
+	public int insertUser(UserVO user) {
 		try {
 			con=DBUtil.getCon();
-			String sql= "insert into memo values(user_seq.nextval,?,?,?)";
+			String sql= "insert into user values(user_seq.nextval,?,?,?)";
 			ps=con.prepareStatement(sql);
 			ps.setString(1, user.getId());
 			ps.setString(2, user.getPw());
@@ -22,20 +23,49 @@ public class BoardDAO {
 			return n;
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return -1;//오류 발생시는 음수를 반환
+			return -1;
 		}finally {
 			close();
 		}
 	}//--------------------------------------
+	
+	public ArrayList<UserVO> Login(){
+		try {
+			con=DBUtil.getCon();
+			String calId = "";
+			String sql = "SELECT idx, id, pw, gr FROM user ORDER BY 1 asc";
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			ArrayList<UserVO> arr = makeList(rs);
+			return arr;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			close();
+		}
+	}
+	
+	public ArrayList<UserVO> makeList(ResultSet rs)
+	throws SQLException{
+		ArrayList<UserVO> arr = new ArrayList<>();
+		while(rs.next()) {
+			int idx = rs.getInt("id");
+			String name = rs.getString("pw");
+			String msg = rs.getString("msg");
+			int gr = rs.getInt("gr");
+			UserVO voTemp = new UserVO(idx, name, msg, gr);
+			arr.add(voTemp);
+		}//while---------------------
+		return arr;
+	}
 	
 	private void close() {
 		try {
 			if(rs!=null) rs.close();
 			if(ps!=null) ps.close();
 			if(con!=null) con.close();
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
+		} catch (Exception e) {}
 	}
 	
 }
