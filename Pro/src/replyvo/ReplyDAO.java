efcda570fe6e9e8d7b23a6dbc257ed695c37c4e3
2class-008 <2class-008@2class-008-PC>
2019-12-03 17:24:02 +0900

    asdf

--- Pro/src/replyvo/ReplyDAO.java
+++ Pro/src/replyvo/ReplyDAO.java
@@ -1,10 +1,52 @@
 package replyvo;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+
+
 
 public class ReplyDAO {
 	Connection con;
 	PreparedStatement ps;
 	ResultSet rs;
+	public ArrayList<ReplyVO> listMemo(){
+		try {
+			con=DBUtil.getCon();
+			String sql="SELECT idx,name,rpad(msg,50,' ') AS msg,wdate"
+					+" FROM memo ORDER BY 1 DESC";
+			ps=con.prepareStatement(sql);
+			rs=ps.executeQuery();
+			ArrayList<ReplyVO> arr=makeList(rs);
+			return arr;
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}finally {
+			close();
+		}
+	}
+
+	public ArrayList<ReplyVO> makeList(ResultSet rs)
+			throws SQLException{
+				ArrayList<ReplyVO> arr=new ArrayList<>();
+				while(rs.next()) {
+					int idx=rs.getInt("idx");
+					String r_content=rs.getString("r_content");
+					java.sql.Date r_wdate=rs.getDate("r_wdate");
+					String id=rs.getString("id");
+					ReplyVO vo=new ReplyVO(idx,r_content,r_wdate,id);
+					arr.add(vo);
+				}////while-----------------------
+				return arr;
+				
+			}
+	
 	
 	public int insertMsg(ReplyVO reply) {
 		try {
