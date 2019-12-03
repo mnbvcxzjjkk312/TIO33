500966e944b6c8d2f490345481a9b070da53c45f
SK O <mnbvcxzjjkk312@hotmail.com>
2019-12-03 15:42:36 +0900

    브랜치

--- Pro/src/BoardUi/BoardDAO.java
+++ Pro/src/BoardUi/BoardDAO.java
@@ -5,8 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import memvo.MEMBERVO;
-import boardvo.BoardVO;
+//import memvo.MEMBERVO;
+//import boardvo.BoardVO;
 
 
 public class BoardDAO
@@ -16,65 +16,65 @@ public class BoardDAO
 	ResultSet rs;
 	
 	
-	public int insertMember(MEMBERVO board) {
-		try {
-			con=DBUtil.getCon();
-			String sql= "insert into member values(board_seq.nextval,?,?,?,?)";
-			ps=con.prepareStatement(sql);
-			ps.setString(1, board.getId());
-			ps.setString(2, board.getPw());
-			ps.setString(3, board.getName());
-			ps.setInt(4, board.getGr());
-			int n = ps.executeUpdate();
-			return n;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return -1;
-		}finally {
-			close();
-		}
-	}//--------------------------------------
+//	public int insertMember(MEMBERVO board) {
+//		try {
+//			con=DBUtil.getCon();
+//			String sql= "insert into member values(board_seq.nextval,?,?,?,?)";
+//			ps=con.prepareStatement(sql);
+//			ps.setString(1, board.getId());
+//			ps.setString(2, board.getPw());
+//			ps.setString(3, board.getName());
+//			ps.setInt(4, board.getGr());
+//			int n = ps.executeUpdate();
+//			return n;
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			return -1;
+//		}finally {
+//			close();
+//		}
+//	}//--------------------------------------
 	
 	
 	
 	
 	//로그인 메소드
-	public ArrayList<BoardVO> Login(){
-		try {
-			con=DBUtil.getCon();
-			String calId = "";
-			String sql = "SELECT member_no, id, password, name, grade FROM board ORDER BY 1 asc";
-			ps = con.prepareStatement(sql);
-			rs = ps.executeQuery();
-			ArrayList<BoardVO> arr = makeList(rs);
-			return arr;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return null;
-		} finally {
-			close();
-		}
-	}
+//	public ArrayList<BoardVO> Login(){
+//		try {
+//			con=DBUtil.getCon();
+//			String calId = "";
+//			String sql = "SELECT member_no, id, password, name, grade FROM board ORDER BY 1 asc";
+//			ps = con.prepareStatement(sql);
+//			rs = ps.executeQuery();
+//			ArrayList<BoardVO> arr = makeList(rs);
+//			return arr;
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			return null;
+//		} finally {
+//			close();
+//		}
+//	}
 
 	
 	//보드를 만드는 메소드
-	public ArrayList<BoardVO> AddBoard(BoardVO board){
-		try {
-			con=DBUtil.getCon();
-			String sql = "INSERT INTO BOARD VALUES(board_seq.nextval,?,?,?,SYSDATE)";
-			ps = con.prepareStatement(sql);
-			ps.setString(1, board.getTitle());
-			ps.setString(2, board.getContent());
-			ps.setString(3, board.getId());
-			int n = ps.executeUpdate();
-			return n;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return null;
-		} finally {
-			close();
-		}
-	}
+//	public ArrayList<BoardVO> AddBoard(BoardVO board){
+//		try {
+//			con=DBUtil.getCon();
+//			String sql = "INSERT INTO BOARD VALUES(board_seq.nextval,?,?,?,SYSDATE)";
+//			ps = con.prepareStatement(sql);
+//			ps.setString(1, board.getTitle());
+//			ps.setString(2, board.getContent());
+//			ps.setString(3, board.getId());
+//			int n = ps.executeUpdate();
+//			return n;
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			return null;
+//		} finally {
+//			close();
+//		}
+//	}
 	
 	// 목록을 클릭하면 본문을 보여주는 메서드.
 	public ArrayList<BoardVO> clickContent(int type)
--- Pro/src/BoardUi/BoardVO.java
+++ Pro/src/BoardUi/BoardVO.java
@@ -1,6 +1,5 @@
 package BoardUi;
 
-<<<<<<< HEAD
 import java.sql.Date;
 
 public class BoardVO
@@ -82,66 +81,4 @@ public class BoardVO
 		return "BoardVO [boardnum =" + boardnum + ", title =" + title + ", content = " + content + ", id =" + id + ", wdate =" + wdate + "]"; 
 	}
 }
-=======
-public class BoardVO {
-	private int idx;//글번호
-	private String id;//작성자
-	private String pw;//메모내용
-	private int gr;//작성일
-	
-	//기본생성자, 인자생성자 => 오버로드
-	
-	public BoardVO() {
-		this(0, null, null, 0);
-	}
 
-	public BoardVO(int idx, String id, String pw, int gr) {
-		super();
-		this.idx = idx;
-		this.id = id;
-		this.pw = pw;
-		this.gr = gr;
-	}
-
-	public int getIdx() {
-		return idx;
-	}
-
-	public void setIdx(int idx) {
-		this.idx = idx;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getPw() {
-		return pw;
-	}
-
-	public void setPw(String pw) {
-		this.pw = pw;
-	}
-
-	public int getGr() {
-		return gr;
-	}
-
-	public void setWGr(int gr) {
-		this.gr = gr;
-	}
-
-	@Override
-	public String toString() {
-		return "BoardVO [idx=" + idx + ", id=" + id + ", pw=" + pw + ","
-				+ " gr=" + gr + "]";
-	}
-	
-	
-}/////////////////////////////////////////////
-
->>>>>>> refs/heads/KDH_B1
